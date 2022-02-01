56d6dfe7f02256136f4f869b9fe327c36ca20828
RobertoGuzman <robertoguzman64@gmail.com>
2022-02-01 20:42:37 +0100

    incio de proyecto con pagina principal hecha

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+const cambiaPantalla = (siguiente) => {
+    let pantallaDeseada = "screen" + siguiente;
+
+    let arrayPantallas = ["screen1","screen2","screen3","screen4","screen5"];
+
+    arrayPantallas = arrayPantallas.filter(Valor => !pantallaDeseada.includes(Valor));
+
+    document.getElementById(pantallaDeseada).style.display = "block";
+
+    for (let pantalla of arrayPantallas){
+
+        document.getElementById(pantalla).style.display = "none";
+    }
+}
