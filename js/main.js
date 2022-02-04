bd6a79fc249296367b6a6c0380be1b22951413b6
RobertoGuzman <robertoguzman64@gmail.com>
2022-02-04 14:30:05 +0100

    Iniciando JavasCript a la pantalla 2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
 
+
+// PANTALLAS
 const cambiaPantalla = (siguiente) => {
     let pantallaDeseada = "screen" + siguiente;
-
     let arrayPantallas = ["screen1","screen2","screen3","screen4"];
-
     arrayPantallas = arrayPantallas.filter(Valor => !pantallaDeseada.includes(Valor));
-
     document.getElementById(pantallaDeseada).style.display = "block";
-
     for (let pantalla of arrayPantallas){
-
         document.getElementById(pantalla).style.display = "none";
+    }
+}
+
+
+
+const selectSoldado = (nSoldado) => {
+    
+    if(equipo1 == ""){
+        equipo1 = todosSoldados[nSoldado];
+        let soldadoPrimero = document.getElementById(nSoldado);
+        let datosSoldado = document.getElementById("data"+ 1);
+        //una vez he escogido el coche, invalido el img para que nadie haga onclick sobre él
+        soldadoPrimero.onclick = "";
+        soldadoPrimero.classList.add("soldadoSeleccionado");
+        datosSoldado.innerHTML = `${equipo1.marca}`;
+       
+    } else if (equipo2 == ""){
+        equipo2 = todosSoldados[nSoldado];
+        let soldadoSegundo = document.getElementById(nSoldado);
+        soldadoSegundo.onclick = "";
+        soldadoSegundo.classList.add("soldadoSeleccionado");
+        //una vez he escogido los dos coches.........
+        setTimeout(()=>{
+            cambiaPantalla(3);
+            // displayGame();
+        },500);
     }
 }
--- js/soldados.js
+++ js/soldados.js
@@ -1,57 +1,36 @@
 
-// CLASE PARA QUE CADA COCHE TENGA CUALIDADES
 
-// class Coche {
-//     constructor(modelo,marca,velocidad,peso,frenado,combustible){
-//         this.modelo = modelo;
-//         this.marca = marca;
-//         this.velocidad = velocidad;
-//         this.peso = peso;
-//         this.frenado = frenado;
-//         this.combustible = combustible;
-//     }
-//     acelerar() {
-//         return this.velocidad += 30;
-//     }
-//     frenar() {
-//         return this.velocidad = 0;
-//     }
-// }
-// // VARIABLES DE COCHES PARA PODER CONCADENARLOS
+class Soldado {
+    constructor(nombre, edad, ataque, defensa){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.ataque = ataque;
+        this.defensa = defensa;
+    }
+    atacar(){
+        return this.ataque += 10;
+    }
+    defender(){
+        return this.defensa += 5;
+    }
 
+}
 
-// let coche1 = new Coche("Cordoba","Seat",250,1800,30,"Diesel");
-// let coche2 = new Coche("Celica","Toyota",260,1900,20,"Gasolina");
-// let coche3 = new Coche("Mustang","Ford",255,1850,25,"Diesel");
-// let coche4 = new Coche("406","Peugeot",257,1950,27,"Gasolina");
-// let coche5 = new Coche("Civic","Honda",245,1875,23,"Diesel");
-// let coche6 = new Coche("Passat","Volkswagen",256,1925,28,"Gasolina");
+let soldado1 = new Soldado ("Cabo", 40, 150,100)
+let soldado2 = new Soldado ("Sargento", 50, 151, 100)
+let soldado3 = new Soldado ("Comandante", 45, 149, 100)
+let soldado4 = new Soldado ("Coronel", 42, 145, 100)
+let soldado5 = new Soldado ("Teniente", 47, 153, 100)
+let soldado6 = new Soldado ("Alférez", 46, 148, 100)
 
-// class Soldado {
-//     constructor(nombre, edad, altura, peso, velocidad){
-//         this.nombre = nombre;
-//         this.edad = edad;
-//         this.altura = altura;
-//         this.peso = peso;
-//         this.velocidad = velocidad;
-//     }
-//     correr(){
-//         return this.velocidad += 10;
-//     }
-//     andar(){
-//         return this.velocidad +=5;
-//     }
-//     agacharse(){
-//         return this.velocidad +=2;
-//     }
-// }
-// class Francotirador extends Soldado{
-//     constructor(nombre, edad, altura, peso, velocidad){
-//         super(nombre, edad, altura, peso, velocidad, alcance)
-//         this.alcance = "muy largo";
-// }
-// class Infanteria extends Soldado{
-//     constructor(nombre, edad, altura, peso, velocidad){
-//         super(nombre, edad, altura, peso, velocidad, alcance)
-//         this.alcance = "medio";
-// }
+let todosSoldados = {
+    1 : soldado1,
+    2 : soldado2,
+    3 : soldado3,
+    4 : soldado4,
+    5 : soldado5,
+    6 : soldado6,
+}
+
+let equipo1 = "";
+let equipo2 = "";
