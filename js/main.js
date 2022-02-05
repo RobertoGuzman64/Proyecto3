be66c8e5178a4eb39cb78c02a6fa8f39bd746bab
Roberto Guzmán Durá <robertoguzman64@gmail.com>
2022-02-05 23:32:58 +0100

    Añadidos algunos comentarios y funcionalidades aun por terminar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 
-// PANTALLAS DEL JUEGO
+//CAMBIO DE PANTALLAS DEL JUEGO
 
 const cambiaPantalla = (siguiente) => {
     let pantallaDeseada = "screen" + siguiente;
@@ -13,53 +13,69 @@ const cambiaPantalla = (siguiente) => {
 }
 
 
-// PANTALLA 2 DEL JUEGO
+// PANTALLA 2 DEL JUEGO (LA SELECCION DE EQUIPOS)
 
 const seleccionarSoldado = (nSoldado) => {
-    
     if(equipo1 == ""){
         equipo1 = todosSoldados[nSoldado];
         let soldadoPrimero = document.getElementById(nSoldado);
         let datosSoldado = document.getElementById("data"+ 1);
-
-        //una vez he escogido el coche, invalido el img para que nadie haga onclick sobre él
-
+        //una vez he escogido el soldado, invalido el img para que nadie haga onclick sobre él
         soldadoPrimero.onclick = "";
         soldadoPrimero.classList.add("soldadoSeleccionado");
 
-        // datosSoldado.innerHTML = `${equipo1.ataque}`;
+        // datosSoldado.innerHTML = `${equipo1.fotoSoldado}`;
+
 
     } else if (equipo2 == ""){
         equipo2 = todosSoldados[nSoldado];
         let soldadoSegundo = document.getElementById(nSoldado);
         soldadoSegundo.onclick = "";
         soldadoSegundo.classList.add("soldadoSeleccionado");
-        
         //una vez he escogido los dos soldados.........
-
         setTimeout(()=>{
             cambiaPantalla(3);
-
             verJuego();
-
         },500);
     }
 }
 
 // PANTALLA 3 DEL JUEGO (LA BATALLA)
 
-// console.log(equipo1,equipo2,"el ganador es....",ganador);
 
 const verJuego = () => {
-    console.log(equipo1,equipo2);
-
+    console.log(equipo1,equipo2,"el ganador es....",ganador);
     verEquipo1.innerHTML = `<img class="foto" style="width:15em;height:25em" src="img/${equipo1.fotoSoldado}.jpg" alt="primer_soldado"/>`;
-    console.log(equipo1);
-
-    // statsTeam1.innerHTML = `<div>${team1.marca}<br>${team1.modelo}<br>${team1.combustible}</div>`;
-
+    estEquipo1.innerHTML = `<div>NOMBRE${equipo1.nombre}<br>EDAD${equipo1.edad}<br>ATAQUE${equipo1.ataque}<br>DEFENSA${equipo1.defensa}<br>${equipo1.vida}</div>`;
     verEquipo2.innerHTML = `<img class="foto" style="width:15;height:25em" src="img/${equipo2.fotoSoldado}.jpg" alt"segundo_soldado"/>`;
+    estEquipo2.innerHTML = `<div>NOMBRE${equipo2.nombre}<br>EDAD${equipo2.edad}<br>ATAQUE${equipo2.ataque}<br>DEFENSA${equipo1.defensa}<br>${equipo1.vida}</div>`;
 }
+
+
+
+
+
+
+const jugar = () => {
+    if((equipo1.vida >= vida_soldado) || (equipo2.vida >= vida_soldado)){
+
+        ganador = equipo1;
+
+        cambiaPantalla(4);
+
+        winner.innerHTML = `${equipo1.fotoSoldado}`;
+
+    }
+
+    equipo1.atacar();
+
+    equipo2.atacar();
+
+    console.log(vida1.innerHTML = `<p>${equipo1.vida}</p>`);
+    console.log(vida2.innerHTML = `<p>${equipo2.vida}</p>`);
+
+}
+
 
 
 
@@ -76,3 +92,8 @@ const verJuego = () => {
 
 let verEquipo1 = document.getElementById("verSoldado1");
 let verEquipo2 = document.getElementById("verSoldado2");
+let estEquipo1 = document.getElementById("est1");
+let estEquipo2 = document.getElementById("est2");
+let vidaEquipo1 = document.getElementById("vida1");
+let vidaEquipo2 = document.getElementById("vida2");
+let winner = document.getElementById("winner");
--- js/soldados.js
+++ js/soldados.js
@@ -1,18 +1,19 @@
 
 // CLASES DE LOS SOLDADOS
 class Soldado {
-    constructor(nombre, edad, ataque, defensa,fotoSoldado){
+    constructor(nombre, edad, ataque, defensa, fotoSoldado){
         this.nombre = nombre;
         this.edad = edad;
         this.ataque = ataque;
         this.defensa = defensa;
         this.fotoSoldado = fotoSoldado;
+        this.vida = 0;
     }
     atacar(){
-        return this.ataque += 10;
+        return this.vida += 100;
     }
     defender(){
-        return this.defensa - 5;
+        return this.ataque - 5;
     }
 }
 
@@ -21,7 +22,7 @@ let soldado2 = new Soldado ("Sargento", 50, 151, 100,"soldado2")
 let soldado3 = new Soldado ("Comandante", 45, 149, 100,"soldado3")
 let soldado4 = new Soldado ("Coronel", 42, 145, 100,"soldado4")
 let soldado5 = new Soldado ("Teniente", 47, 153, 100,"soldado5")
-let soldado6 = new Soldado ("Alférez", 46, 148, 100,"soldado6")
+let soldado6 = new Soldado ("Zombie", 105, 148, 100,"soldado6")
 
 let todosSoldados = {
     1 : soldado1,
@@ -35,5 +36,5 @@ let todosSoldados = {
 let equipo1 = "";
 let equipo2 = "";
 
-// let ganador = "";
-
+let ganador = "";
+let vida_soldado = 500;
